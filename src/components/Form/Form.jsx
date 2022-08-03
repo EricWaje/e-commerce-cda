36b750de60c5916d83f858194ad5af2e509158c7
Eric Wajnrajch <ericwaje03@gmail.com>
2022-08-03 12:26:53 -0300

    terminando estilos

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,64 +1,19 @@
 import { useState } from 'react';
 
 const Form = () => {
-    // const [nombre, setNombre] = useState('');
-    // const [email, setEmail] = useState('');
-    // const [telefono, setTelefono] = useState('');
-    //const [coord, setCoord] = useState({ x: '', y: '' });
-    const [dataForm, setDataForm] = useState({
-        nombre: '',
-        email: '',
-        telefono: '',
-    });
+    const [user, setUser] = useState('');
+    const [phone, setPhone] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        // const nombre = e.target.elements.nombre.value;
-        // const email = e.target.elements.email.value;
-        // const telefono = e.target.elements.telefono.value;
-        // const data = {
-        //     nombre,
-        //     email,
-        //     telefono,
-        // };
-
-        console.log(dataForm);
     };
 
-    /* const handleChangeName = (e) => {
-        setNombre(e.target.value);
+    const handleChangeUser = (e) => {
+        setUser(e.target.value);
     };
-    const handleChangeEmail = (e) => {
-        setEmail(e.target.value);
+    const handleChangePhone = (e) => {
+        setPhone(e.target.value);
     };
-    const handleChangeTelefono = (e) => {
-        setTelefono(e.target.value);
-    }; */
-
-    const handleChange = (e) => {
-        //console.log(e.target.name);
-        //console.log(e.target.value);
-        const { name, value } = e.target;
-        setDataForm({
-            ...dataForm,
-            [name]: value,
-        });
-    };
-
-    //console.log(dataForm);
-
-    /* useEffect(() => {
-        const handleMouse = (e) => {
-            setCoord({
-                ...coord,
-                x: e.clientX,
-                y: e.clientY,
-            });
-        };
-        window.addEventListener('mousemove', handleMouse);
-        return () => {
-            window.removeEventListener('mousemove', handleMouse);
-        };
-    }); */
 
     return (
         <div>
@@ -67,28 +22,18 @@ const Form = () => {
                     type="text"
                     name="nombre"
                     placeholder="Ingresa tu nombre"
-                    onChange={handleChange}
-                    value={dataForm.nombre}
-                />
-                <input
-                    type="text"
-                    name="email"
-                    placeholder="Ingresa tu email"
-                    onChange={handleChange}
-                    value={dataForm.email}
+                    onChange={handleChangeUser}
+                    value={user}
                 />
                 <input
                     type="text"
                     name="telefono"
                     placeholder="Ingresa tu telefono"
-                    onChange={handleChange}
-                    value={dataForm.telefono}
+                    onChange={handleChangePhone}
+                    value={phone}
                 />
                 <button>Enviar</button>
             </form>
-
-            {/* <h1>X: {coord.x}</h1>
-            <h1>Y: {coord.y}</h1> */}
         </div>
     );
 };
